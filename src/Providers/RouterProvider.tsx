115ef839d4690c9fa36f4ff765fb7117a22af244
koryun <koryun.ohanyan@gmail.com>
2021-09-06 21:35:28 +0400

    initial commit

--- src/Providers/RouterProvider.tsx
+++ src/Providers/RouterProvider.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Homepage from '../pages/Homepage'
-import SignUp from '../pages/SignUp'
-import SignIn from '../pages/SignIn'
-import Bedroom from '../pages/Bedroom'
-import Bathroom from '../pages/Bathroom'
+// import Homepage from '../pages/Homepage'
+// import SignUp from '../pages/SignUp'
+// import SignIn from '../pages/SignIn'
+// import Bedroom from '../pages/Bedroom'
+// import Bathroom from '../pages/Bathroom'
 import Catalogue from '../pages/Catalogue'
 
 const routes = [
-  {component: Homepage, path: '/'},
-  {component: SignUp, path: '/sign-up'},
-  {component: SignIn, path: '/sign-in'},
-  {component: Bedroom, path: '/bedroom'},
-  {component: Bathroom, path: '/bedroom'},
+  // {component: Homepage, path: '/'},
+  // {component: SignUp, path: '/sign-up'},
+  // {component: SignIn, path: '/sign-in'},
+  // {component: Bedroom, path: '/bedroom'},
+  // {component: Bathroom, path: '/bedroom'},
   {component: Catalogue, path: '/catalogue'},
 ]
 
