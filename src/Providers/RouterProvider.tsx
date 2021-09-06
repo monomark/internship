7dae7c09e4ee8eeddd4a212e8b0ac5e950e6c308
ManookDavid <manookdavid@gmail.com>
2021-09-06 20:36:56 +0400

    created routing with react-router-dom

---
+++ src/Providers/RouterProvider.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Homepage from '../pages/Homepage'
+import SignUp from '../pages/SignUp'
+import SignIn from '../pages/SignIn'
+import Bedroom from '../pages/Bedroom'
+import Bathroom from '../pages/Bathroom'
+import Catalogue from '../pages/Catalogue'
+
+const routes = [
+  {component: Homepage, path: '/'},
+  {component: SignUp, path: '/sign-up'},
+  {component: SignIn, path: '/sign-in'},
+  {component: Bedroom, path: '/bedroom'},
+  {component: Bathroom, path: '/bedroom'},
+  {component: Catalogue, path: '/catalogue'},
+]
+
+function RouterProvider() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {
+          routes.map(item => (
+            <Route
+              exact
+              path={item.path}
+              component={item.component}/>
+          ))
+        }
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default RouterProvider;
