02eaaa7467055a41fdace3e7a7d08c0d5455005c
ManookDavid <manookdavid@gmail.com>
2021-09-06 20:18:49 +0400

    created chakra theme with button component

---
+++ src/theme/Button.tsx
@@ -0,0 +1,31 @@
+export const Button = {
+    baseStyles: {
+        
+    },
+    variants: {
+        'pink': {
+
+        },
+        'transparent': {
+
+        }
+    },
+    sizes: {
+        sm: {
+
+        },
+        md: {
+
+        },
+        lg: {
+
+        },
+        xl: {
+
+        }
+    },
+    defaultProps: {
+        size: 'md',
+        variant: 'transparent'
+    }
+}
