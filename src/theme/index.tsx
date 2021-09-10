48dd11e18cd2c6766a57654f8e75bfbb6eaf7e25
Gor <gorgasparyann95@gmail.com>
2021-09-10 18:04:24 +0400

    create Button theme

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,27 +1,44 @@
-import { extendTheme } from '@chakra-ui/react'
-import { Button } from './Button'
+import { extendTheme } from "@chakra-ui/react";
+import { Button } from "./Button";
 
 const theme = extendTheme({
-    styles: {
-        global: {
-            'html, body': {
-                background: '#fff',
-                color: '#000'
-            }
-        }
+  styles: {
+    global: {
+      "html, body": {
+        background: "#3C3C3C",
+        color: "#000",
+      },
     },
-    fonts: {
-        body: 'Montserrat, sans-serif',
-        heading: 'Montserrat, sans-serif'
+  },
+  fonts: {
+    body: "Montserrat, sans-serif",
+    heading: "Montserrat, sans-serif",
+  },
+  components: {
+    Button,
+    FormLabel: {
+      baseStyle: {
+        fontWeight: "bold",
+        fontSize: "15px",
+        lineHeight: "18px",
+        color: "#616161",
+        textTransform: "uppercase",
+      },
     },
-    components: {
-        Button,
+    Text: {
+      baseStyle: {
+        fontWeight: "bold",
+        fontSize: "25px",
+        lineHeight: "30px",
+        color: "#3C3C3C",
+      },
     },
-    colors: {
-        grey: {
-            100: "#c4c4c4",
-        },
-    }
-})
+  },
+  colors: {
+    grey: {
+      100: "#c4c4c4",
+    },
+  },
+});
 
-export default theme
+export default theme;
