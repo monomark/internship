155fa1261db964780b43d821e532fd149845a5dd
Gor <gorgasparyann95@gmail.com>
2021-09-10 18:03:24 +0400

    create routeing

--- src/Providers/RouterProvider.tsx
+++ src/Providers/RouterProvider.tsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Homepage from "../pages/Homepage";
-import SignUp from "../pages/SignUp";
-// import SignIn from '../pages/SignIn'
-// import Bedroom from '../pages/Bedroom'
-// import Bathroom from '../pages/Bathroom'
-// import Catalogue from '../pages/Catalogue'
+import Signup from "../pages/Signup";
+// // import SignIn from '../pages/SignIn'
+// // import Bedroom from '../pages/Bedroom'
+// // import Bathroom from '../pages/Bathroom'
+// // import Catalogue from '../pages/Catalogue'
 
 const routes = [
   { component: Homepage, path: "/" },
-  { component: SignUp, path: "/sign-up" },
-  // {component: SignIn, path: '/sign-in'},
-  // {component: Bedroom, path: '/bedroom'},
-  // {component: Bathroom, path: '/bedroom'},
-  // {component: Catalogue, path: '/catalogue'},
+  { component: Signup, path: "/sign-up" },
+  //   // {component: SignIn, path: '/sign-in'},
+  //   // {component: Bedroom, path: '/bedroom'},
+  //   // {component: Bathroom, path: '/bedroom'},
+  //   // {component: Catalogue, path: '/catalogue'},
 ];
 
 function RouterProvider() {
