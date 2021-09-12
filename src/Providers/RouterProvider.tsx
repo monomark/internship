2efa9664ff13b8cbee85c3559c423959f04cbdd5
koryun <koryun.ohanyan@gmail.com>
2021-09-12 14:44:15 +0400

    papki anune mecatar

--- src/Providers/RouterProvider.tsx
+++ src/Providers/RouterProvider.tsx
@@ -4,7 +4,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 // import SignIn from '../pages/SignIn'
 // import Bedroom from '../pages/Bedroom'
 // import Bathroom from '../pages/Bathroom'
-import Catalogue from '../pages/Catalogue'
+import Catalogue from "../pages/Catalogues";
+import OnClikKitchen from "../pages/Catalogues/OnClikKitchen";
 
 const routes = [
   // {component: Homepage, path: '/'},
@@ -12,21 +13,18 @@ const routes = [
   // {component: SignIn, path: '/sign-in'},
   // {component: Bedroom, path: '/bedroom'},
   // {component: Bathroom, path: '/bedroom'},
-  {component: Catalogue, path: '/catalogue'},
-]
+  { component: Catalogue, path: "/catalogue" },
+  { component: OnClikKitchen, path: "/kitchen" },
+
+];
 
 function RouterProvider() {
   return (
     <BrowserRouter>
       <Switch>
-        {
-          routes.map(item => (
-            <Route
-              exact
-              path={item.path}
-              component={item.component}/>
-          ))
-        }
+        {routes.map((item) => (
+          <Route exact path={item.path} component={item.component} />
+        ))}
       </Switch>
     </BrowserRouter>
   );
