ecdc8e57e92fa07a0beb0077957d8fff603a935b
Hayk <hayksargsyan0909@gmail.com>
2021-09-13 03:47:44 +0300

    delete signin/button/moved to inputs component,add/link/text,change/hstack/spacing,add /link/svg/</link

--- src/pages/SignIn/Buttonicon.tsx
+++ src/pages/SignIn/Buttonicon.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { VStack, Button, Text, HStack, Link } from "@chakra-ui/react";
+import { VStack, Text, HStack, Link } from "@chakra-ui/react";
 import { ReactComponent as Facebook } from "../../assets/facebook.svg";
 import { ReactComponent as Instagram } from "../../assets/instagram.svg";
 import { ReactComponent as Vk } from "../../assets/vk.svg";
@@ -7,23 +7,26 @@ import { ReactComponent as Google } from "../../assets/GOOGLE.svg";
 
 const Buttonicon = () => {
   return (
-    <VStack pt="2rem">
-      <Button variant="pink" w="260px" h="35px">
-        SIGN IN
-      </Button>
-      <Text fontSize="sm">Forgot password?</Text>
+    <VStack pt="1rem">
+      <Link>
+        <Text fontSize="sm">Forgot password?</Text>
+      </Link>
       <Text fontSize="3xl" fontWeight="700" pt="4rem">
         Or sign up with...
       </Text>
-      <HStack spacing="1.8rem" pt="3rem">
-        <Link href="https://www.facebook.com/" isExternal />
-        <Facebook />
-        <Link href="https://www.instagram.com/" isExternal />
-        <Instagram />
-        <Link href="https://vk.com/" isExternal />
-        <Vk />
-        <Link href="https://myaccount.google.com/" isExternal />
-        <Google />
+      <HStack spacing="4rem" pt="3rem">
+        <Link href="https://www.facebook.com/" isExternal>
+          <Facebook />
+        </Link>
+        <Link href="https://www.instagram.com/" isExternal>
+          <Instagram />
+        </Link>
+        <Link href="https://vk.com/" isExternal>
+          <Vk />
+        </Link>
+        <Link href="https://myaccount.google.com/" isExternal>
+          <Google />
+        </Link>
       </HStack>
     </VStack>
   );
