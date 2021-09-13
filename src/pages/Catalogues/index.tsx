03d490282f5f928dfbf36cadacf76e4f52c619e6
koryun <koryun.ohanyan@gmail.com>
2021-09-13 21:01:41 +0400

    add exact

--- src/pages/Catalogues/index.tsx
+++ src/pages/Catalogues/index.tsx
@@ -1,5 +1,5 @@
 import { Flex, Container, Box } from "@chakra-ui/react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Buttons from "./Buttons";
 import Footer from "./Footer";
 import Pics from "./Pics";
@@ -10,25 +10,24 @@ import OnCllickKitchen from "./OnClikKitchen";
 
 const Catalogue = () => {
   return (
-   <BrowserRouter>
     <Container maxW="6xl">
-    <Header />
+      <Header />
       <Buttons />
-   { <Switch>
-  <Route exact path="/kitchen" component={OnCllickKitchen}/>
-   </Switch> 
-   }
-     
-     <Box pt="16">
-       <Flex alignItems="center" justifyContent="space-between">
-         <Pics />
-         <Pics2 />
-         <Pics3 />
-       </Flex>
-     </Box>
-     <Footer />
-   </Container>
-   </BrowserRouter>
+      {
+        <Switch>
+          <Route path="/catalogue/kitchen" component={OnCllickKitchen} />
+        </Switch>
+      }
+
+      <Box pt="16">
+        <Flex alignItems="center" justifyContent="space-between">
+          <Pics />
+          <Pics2 />
+          <Pics3 />
+        </Flex>
+      </Box>
+      <Footer />
+    </Container>
   );
 };
 
