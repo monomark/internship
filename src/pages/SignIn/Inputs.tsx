591330198337c0957c220ab8dc612e015b57aaa5
Hayk <hayksargsyan0909@gmail.com>
2021-09-13 03:36:43 +0300

    install react-hook-form add useform/submithandler/ button/submit/consolelog/email/password

--- src/pages/SignIn/Inputs.tsx
+++ src/pages/SignIn/Inputs.tsx
@@ -1,21 +1,55 @@
 import React from "react";
-import { VStack, FormControl, FormLabel, Input, Flex } from "@chakra-ui/react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import {
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  VStack,
+} from "@chakra-ui/react";
 
-const Inputs = () => {
-  return (
-    <Flex justifyContent="center" pt="2rem">
-      <VStack>
-        <FormControl id="email">
-          <FormLabel>EMAIL ADRESS:</FormLabel>
-          <Input type="email" w="330px" h="60px" bg="grey.300" mb="1.5rem" />
-        </FormControl>
-        <FormControl id="password">
-          <FormLabel>PASSWORD:</FormLabel>
-          <Input type="password" bg="grey.300" h="60px" />
-        </FormControl>
-      </VStack>
-    </Flex>
-  );
+type FormValues = {
+  email: string;
+  password: string | number;
 };
 
+function Inputs() {
+  const { register, handleSubmit } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
+
+  return (
+    <Flex justifyContent="center" pt="2rem">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl id="email">
+          <FormLabel>EMAIL ADRESS:</FormLabel>
+          <Input
+            type="email"
+            {...register("email")}
+            w="330px"
+            h="60px"
+            bg="grey.300"
+            mb="1.5rem"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>PASSWORD:</FormLabel>
+          <Input
+            type="password"
+            {...register("password")}
+            w="330px"
+            h="60px"
+            bg="grey.300"
+            mb="1.5rem"
+          />
+        </FormControl>
+        <VStack>
+          <Button type="submit" variant="pink" minW="260px" h="35px">
+            SIGN IN
+          </Button>
+        </VStack>
+      </form>
+    </Flex>
+  );
+}
 export default Inputs;
