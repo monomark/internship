21933825f671bbda1e8fa4d9af13760aad43e513
minas <minasmalxas@gmail.com>
2021-09-13 14:50:32 +0400

    added functionality for buton click

--- src/pages/Bedroom/Search.tsx
+++ src/pages/Bedroom/Search.tsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 
 function Search() {
+  const [values, setValues] = useState({
+    field: "",
+  });
+
   const handleKeyDown = (evt: any) => {
     if (evt.keyCode === 13) {
       alert(evt.target.value);
     }
   };
 
+  function inputChangeValue({ target: { name, value } }: any) {
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  }
+
+  const handleClick = () => {
+    alert(values.field);
+  };
+
   return (
     <InputGroup width="340px" style={{ marginLeft: "900px" }}>
-      <Input pr="50px" placeholder="Type something" onKeyDown={handleKeyDown} />
+      <Input
+        pr="50px"
+        placeholder="Type something"
+        name="field"
+        onKeyDown={handleKeyDown}
+        onChange={inputChangeValue}
+        onClick={handleClick}
+        value={values.field}
+      />
       <InputRightElement width="80px">
         <Button
           size="sm"
