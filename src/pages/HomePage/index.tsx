7c97c03449e97bcaa1931f22fdbb8e5788fe0957
Hayk <hayksargsyan0909@gmail.com>
2021-09-15 16:57:51 +0300

    create homepage/index component/name homepage

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Box, Container, Flex, Heading, Text, Button } from "@chakra-ui/react";
+import Navbar from "./Navbar";
+// import Header from "../HomePage/Header";
+
+const HomePage = () => {
+  return (
+    <Box w="full" minH="100vh ">
+      <Container position="relative" maxW="7xl">
+        <Navbar />
+        <Flex w="full" align="center" justifyContent="center" minH="100vh">
+          <Flex w="full" align="center" justifyContent="space-between">
+            <Box>
+              <Heading>Your dream</Heading>
+              <Text>here some text</Text>
+              <Button w="200px" variant="blue">
+                View catalog
+              </Button>
+            </Box>
+          </Flex>
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
+
+export default HomePage;
