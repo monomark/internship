1ec489c44b583e1cf7a51b6fbf5d8e26f35f5cae
Hayk <hayksargsyan0909@gmail.com>
2021-09-15 17:01:15 +0300

    create homepage/swip comp

---
+++ src/pages/HomePage/Swip.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Image, Box } from "@chakra-ui/react";
+
+const Swip = () => {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+    >
+      <SwiperSlide>Slide 1 </SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4 </SwiperSlide>
+    </Swiper>
+  );
+};
+export default Swip;
+
+//  error typscript can not module swiper?
