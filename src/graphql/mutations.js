0e83dbd226e21e135f6364343479bd213e6353e6
ManookDavid <manookdavid@gmail.com>
2021-09-22 20:57:36 +0400

    setuped amplify

---
+++ src/graphql/mutations.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createUser = /* GraphQL */ `
+  mutation CreateUser(
+    $input: CreateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    createUser(input: $input, condition: $condition) {
+      id
+      email
+      phone_number
+      avatar
+      first_name
+      last_name
+      age
+      country
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateUser = /* GraphQL */ `
+  mutation UpdateUser(
+    $input: UpdateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    updateUser(input: $input, condition: $condition) {
+      id
+      email
+      phone_number
+      avatar
+      first_name
+      last_name
+      age
+      country
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteUser = /* GraphQL */ `
+  mutation DeleteUser(
+    $input: DeleteUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    deleteUser(input: $input, condition: $condition) {
+      id
+      email
+      phone_number
+      avatar
+      first_name
+      last_name
+      age
+      country
+      createdAt
+      updatedAt
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,51 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
+      id
+      email
+      phone_number
+      avatar
+      first_name
+      last_name
+      age
+      country
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
+      id
+      email
+      phone_number
+      avatar
+      first_name
+      last_name
+      age
+      country
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
+      id
+      email
+      phone_number
+      avatar
+      first_name
+      last_name
+      age
+      country
+      createdAt
+      updatedAt
+    }
+  }
+`;
