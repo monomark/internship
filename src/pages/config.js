1eed14a5ac68f9c12320336072858a74e92065ea
Gor <gorgasparyann95@gmail.com>
2021-09-29 17:51:42 +0400

    add pages in routes

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,21 +1,49 @@
-import AuthLayout from '../layouts/AuthLayout'
-import DefaultLayout from '../layouts/DefaultLayout'
-import Homepage from './Homepage'
-import Register from './Register'
+import AuthLayout from "../layouts/AuthLayout";
+import DefaultLayout from "../layouts/DefaultLayout";
+import Homepage from "./Homepage";
+import Register from "./Register";
+import LogIn from "./LogIn";
+import Verify from "./Verify";
+import ForgottenPassword from "./ForgottenPassword";
+import ResetPassword from "./ResetPassword";
 
 const config = [
-    {
-        component: Homepage,
-        path: '/',
-        secure: false,
-        layout: DefaultLayout,
-    },
-    {
-        component: Register,
-        path: '/register',
-        secure: false,
-        layout: AuthLayout,
-    }
-]
+  {
+    component: Homepage,
+    path: "/",
+    secure: false,
+    layout: DefaultLayout,
+  },
+  {
+    component: Register,
+    path: "/register",
+    secure: false,
+    layout: AuthLayout,
+  },
+  {
+    component: LogIn,
+    path: "/login",
+    secure: false,
+    layout: AuthLayout,
+  },
+  {
+    component: Verify,
+    path: "/verify",
+    secure: false,
+    layout: AuthLayout,
+  },
+  {
+    component: ForgottenPassword,
+    path: "/forgotpassword",
+    secure: false,
+    layout: AuthLayout,
+  },
+  {
+    component: ResetPassword,
+    path: "/resetpassword",
+    secure: false,
+    layout: AuthLayout,
+  },
+];
 
-export default config
+export default config;
