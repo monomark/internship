159ed12cc627d73f372a858d725bd8550dafa238
ManookDavid <manookdavid@gmail.com>
2021-09-29 20:19:12 +0400

    BUtton and theme fixes

--- src/theme/index.js
+++ src/theme/index.js
@@ -6,24 +6,36 @@ const theme = extendTheme({
   styles: {
     global: {
       "html, body": {
-        fontSize: {},
+        fontSize: '1rem',
+        color: '#000',
+        },
       },
     },
-  },
-
   fonts: {
-    body: "Ephesis",
-    heading: "Ephesis",
+    body: "Ephesis, sans-serif",
+    heading: "Ephesis, sans-serif",
   },
   components: {
     Select: {
-      baseStyle: {},
+      baseStyle: {
+        borderRadius: "8px",
+      },
     },
     Input: {
       baseStyle: {
         borderRadius: "8px",
       },
     },
+  },
+  fontSizes: {
+    xs: "0.625rem",
+    sm: "0.75rem",
+    md: "0.875rem",
+    lg: "1rem",
+    xl: "1.125rem",
+    "2xl": "1.25rem",
+    "3xl": "1.5rem",
+    "4xl": "2.0rem",
   },
 });
 
