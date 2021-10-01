2777ca79609c36b87495842a83a76fd96f0381c2
Gor <gorgasparyann95@gmail.com>
2021-10-01 12:34:10 +0400

    create Verify logic

--- src/pages/Verify/index.jsx
+++ src/pages/Verify/index.jsx
@@ -1,34 +1,51 @@
 import React, { useState } from "react";
 import { Input, Button, Flex, Box, VStack, Image } from "@chakra-ui/react";
 import { Auth } from "aws-amplify";
+import { useLocation, useHistory, Link } from "react-router-dom";
 
 const Verify = () => {
   const [value, setValue] = useState("");
+  const history = useHistory();
+  const params = new URLSearchParams(useLocation().search);
+  const [loading, setLoading] = useState(false);
 
   const submit = async (event) => {
     event.preventDefault();
+    const username = params.get("username");
     try {
+      setLoading(true);
       await Auth.confirmSignUp({
+        username,
         value,
       });
+      history.push("/login");
     } catch (e) {
+      setLoading(false);
       console.log("error confirming register", e);
     }
   };
 
   return (
     <>
-      <Image src="/assets/mobile.png" maxW="600px" h="100vh" />
+      <Image src="/assets/mobile.png" maxW="600px" h="100vh" w="full" />
       <Flex justifyContent="center" w="full">
         <Box w="full" maxW="600px">
           <form onsubmit={submit}>
-            <VStack px="8" spacing="5">
+            <VStack px="8" spacing="5" w="full">
+              <Link to="/">
+                <Image src="/assets/logosh.png" maxW="300px" w="full" />
+              </Link>
               <Input
                 placeholder="write code"
                 value={value}
                 onChange={(event) => setValue(event.target.value)}
               />
-              <Button>Verify</Button>
+              <Button type="submit" variant="red" isLoading={loading}>
+                Verify
+              </Button>
+              <Link to="/register">
+                <Button variant="red">Register</Button>
+              </Link>
             </VStack>
           </form>
         </Box>
