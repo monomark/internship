8c7bac675d13d98b883a2060d4fda10e098077f0
Gor <gorgasparyann95@gmail.com>
2021-10-01 12:33:51 +0400

    create resetpassword logic

--- src/pages/ResetPassword/index.jsx
+++ src/pages/ResetPassword/index.jsx
@@ -9,43 +9,56 @@ import {
   Image,
 } from "@chakra-ui/react";
 import { Auth } from "aws-amplify";
+import { useHistory, Link } from "react-router-dom";
+import { useUser } from "../../hooks";
 
 const ResetPassword = () => {
   const [value, setValue] = useState("");
+  const [password, setPassword] = useState("");
+  const { userObject, setUserObject } = useUser();
+  const history = useHistory();
 
   const submit = async (event) => {
     event.preventDefault();
-    try {
-      await Auth.completeNewPassword({
-        // user,
-        // newPassword,
-      });
-    } catch (e) {
-      console.log("error password", e);
-    }
+    Auth.forgotPasswordSubmit({
+      username: userObject.username,
+      value,
+      new_password: password,
+    })
+      .then(({ data }) => setUserObject({ user: data.getUser, loading: false }))
+      .catch((e) => console.log("Invalid passsword", e));
+    history.push("./login");
   };
 
   return (
     <>
-      <Image src="/assets/reset.png" maxW="600px" h="100vh" />
+      <Image src="/assets/reset.png" maxW="600px" h="100vh" w="full" />
       <Flex justifyContent="center" w="full">
         <Box w="full" maxW="600px">
           <form onsubmit={submit}>
-            <VStack px="8" spacing="5">
+            <VStack px="8" spacing="5" w="full">
+              <Link to="/">
+                <Image src="/assets/logosh.png" maxW="300px" w="full" />
+              </Link>
               <Text fontSize="20px" fontWeight="bold">
                 Please enter new password
               </Text>
               <Input
-                placeholder="new password"
+                placeholder="code"
                 value={value}
                 onChange={(event) => setValue(event.target.value)}
               />
               <Input
-                placeholder="repeat password"
-                value={value}
-                onChange={(event) => setValue(event.target.value)}
+                placeholder="new password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
               />
-              <Button type="submit">Submit</Button>
+              <Button type="submit" variant="red">
+                Submit
+              </Button>
+              <Link to="/forgotpassword">
+                <Button>forgotPassword?</Button>
+              </Link>
             </VStack>
           </form>
         </Box>
