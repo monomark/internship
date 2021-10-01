609b202c2adebfd1ac5b3b42cb9d4df9a0ce25f7
Gor <gorgasparyann95@gmail.com>
2021-10-01 12:33:00 +0400

    forgotpassword logic

--- src/pages/ForgottenPassword/index.jsx
+++ src/pages/ForgottenPassword/index.jsx
@@ -9,26 +9,33 @@ import {
   Image,
 } from "@chakra-ui/react";
 import { Auth } from "aws-amplify";
+import { useUser } from "../../hooks";
+import { useHistory, Link } from "react-router-dom";
 
 const ForgottenPassword = () => {
   const [value, setValue] = useState("");
+  const { userObject } = useUser();
+  const history = useHistory();
+  console.log(userObject);
 
   const submit = async (event) => {
     event.preventDefault();
-    try {
-      await Auth.forgotPassword();
-    } catch (e) {
-      console.log("forgot password", e);
-    }
+    Auth.forgotPassword({ username: userObject.username })
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+    history.push("/resetpassword");
   };
 
   return (
     <>
-      <Image src="/assets/forgotpassword.png" maxW="600px" h="100vh" />
+      <Image src="/assets/forgotpassword.png" maxW="600px" h="100vh" w="full" />
       <Flex justifyContent="center" w="full">
         <Box w="full" maxW="600px">
           <form onsubmit={submit}>
-            <VStack px="8" spacing="5">
+            <VStack px="8" spacing="5" w="full">
+              <Link to="/">
+                <Image src="/assets/logosh.png" maxW="300px" w="full" />
+              </Link>
               <Text fontSize="20px" fontWeight="bold">
                 Please enter the email that you used to sign in
               </Text>
@@ -37,7 +44,12 @@ const ForgottenPassword = () => {
                 value={value}
                 onChange={(event) => setValue(event.target.value)}
               />
-              <Button type="submit">Reset Password</Button>
+              <Button type="submit" variant="red">
+                Reset Password
+              </Button>
+              <Link to="/login">
+                <Button variant="red">Log In</Button>
+              </Link>
             </VStack>
           </form>
         </Box>
