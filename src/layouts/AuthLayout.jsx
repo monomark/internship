7752260ebfa0f3efb54b099c951ff8055350d1c3
Gor <gorgasparyann95@gmail.com>
2021-10-06 14:55:06 +0400

    add responsive direction

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { Flex } from "@chakra-ui/react";
 
 const AuthLayout = ({ children }) => (
-  <Flex alignItems="center" minHeight="100vh" width="100%">
+  <Flex
+    direction={["column", "column", "row"]}
+    alignItems="center"
+    minHeight="100vh"
+    width="100%"
+  >
     {children}
   </Flex>
 );
 
 export default AuthLayout;
-
