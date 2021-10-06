86c9b030b03172ad5bdbed934ec28559f1ec467c
Hayk <hayksargsyan0909@gmail.com>
2021-10-06 17:51:48 +0300

    create profile/component

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,45 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import {
-  Flex,
-  Box,
-  Text,
-  Button,
-  ListItem,
-  UnorderedList,
-} from '@chakra-ui/react';
-import { useUser } from '../../hooks'
-import { Link } from 'react-router-dom'
-import FileUploud from './FileUpload'
+import React from "react";
+import { Flex, HStack } from "@chakra-ui/react";
+import FileUploud from "./FileUpload";
+import UserInfo from "./UserInfo";
 
 const Profile = () => {
-  const [todos, setTodos] = useState()
-  const { user, loading } = useUser()
-  if (!user) return <></>
+  // if (!user) return <></>;
   return (
-    <Flex>
-      <Flex justifyContent="center" w="full">
-        <Box color="blue.300">
+    <Flex px="60px" maxw="100vh" mt="150px">
+      <Flex w="full">
+        <HStack spacing="6rem">
           <FileUploud />
-            <Box fontSize="4xl">
-              <UnorderedList>
-                {/* <Img src={items.avatar} alt="user" h="50px" /> */}
-                <ListItem>Age: {user.age}</ListItem>
-                <ListItem>LastName: {user.last_name}</ListItem>
-                <ListItem>Country: {user.country}</ListItem>
-                <ListItem>PhoneNumber: {user.phone_number}</ListItem>
-                <ListItem>Email: {user.email}</ListItem>
-                <ListItem>CreateDate: {user.createdAt}</ListItem>
-              </UnorderedList>
-            </Box>
-        </Box>
-      </Flex>
-      <Flex py="4" w="full" position="absolute" bottom="0" justifyContent="space-between" bg="blue.300" px="100px">
-        <Link to="/">
-          <Button variant="blue">Back</Button>
-        </Link>
-        <Link to="/forgotpassword">
-          <Button variant="blue">Cange password</Button>
-        </Link>
+          <UserInfo />
+        </HStack>
       </Flex>
     </Flex>
   );
