ef53016bc37363b32d70601a516d9b5cb10e346a
ManookDavid <manookdavid@gmail.com>
2021-10-06 19:58:07 +0400

    login responsive

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -3,7 +3,6 @@ import { Flex } from "@chakra-ui/react";
 
 const AuthLayout = ({ children }) => (
   <Flex
-    direction={["column", "column", "row"]}
     alignItems="center"
     minHeight="100vh"
     width="100%"
--- src/pages/LogIn/index.jsx
+++ src/pages/LogIn/index.jsx
@@ -54,7 +54,7 @@ const Login = () => {
 
   return (
     <>
-      <Image src="/assets/buy1.png" maxW="600px" h="100vh" w="full" />
+      <Image display={{base: 'none', md: 'block'}} objectFit="contain" src="/assets/buy1.png" maxW="400px" h="100vh" w="full" />
       <Flex justifyContent="center" w="full">
         <Box w="full" maxW="600px">
           <form noValidate onSubmit={handleSubmit(submit)}>
