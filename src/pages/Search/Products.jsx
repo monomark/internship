b91034dedae8aa415f919e6c782248cde4d5f084
DavidManukyan <55136961+DavidManookyan@users.noreply.github.com>
2021-10-06 19:44:54 +0400

    created search filter (#30)
    Co-authored-by: minas <minasmalxas@gmail.com>

--- src/pages/Search/Products.jsx
+++ src/pages/Search/Products.jsx
@@ -2,29 +2,28 @@ import { SimpleGrid, GridItem } from "@chakra-ui/react";
 import Card from "../../components/Card";
 
 const Best = [
-  { name: "first", price: 3700, type: 'Wood'  },
-  { name: "second", price: 50000, type:  'Metal' },
-  { name: "third", price: 1000, type: 'Other'  },
-  { name: "forth", price: 15000, type:  'Plastic' },
+  { name: "first", price: 3700, type: "Wood" },
+  { name: "second", price: 50000, type: "Metal" },
+  { name: "third", price: 1000, type: "Other" },
+  { name: "forth", price: 15000, type: "Plastic" },
 ];
 
-const Products = () => {
+const Products = ({ search }) => {
   return (
     <SimpleGrid
-        w="full"
-        columns={{
+      w="full"
+      columns={{
         base: 1,
         md: 2,
         xl: 3,
-        }}
-        spacing="6"
+      }}
+      spacing="6"
     >
-      {Best.map((item) => (
+      {Best.filter((item) => item.name.includes(search)).map((item) => (
         <GridItem key={item.name}>
           <Card value={item} />
         </GridItem>
       ))}
-  
     </SimpleGrid>
   );
 };
