93773cb496616abced88503a79f8677bd4044559
Gor <gorgasparyann95@gmail.com>
2021-10-08 19:18:51 +0400

    add new page

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,34 +1,26 @@
-import {
-    Text,
-    Image,
-    Stack,
-    Heading,
-    Box,
-} from "@chakra-ui/react";
+import { Text, Image, Stack, Heading, Box } from "@chakra-ui/react";
 
-const Card = ({value}) => {
+const Card = ({ value }) => {
   return (
-      <Box
-        borderRadius="8px"
-        overflow="hidden"
-        boxShadow="0 0 24px 4px rgba(0, 0, 0, 0.15)">
-        <Image
-            src="/assets/images/photo.jpg"
-            w="full"
-            objectFit="cover"
-            h="200px" />
-        <Stack
-            p="4"
-            bg="yellow.100">
-            <Heading
-                fontSize="3xl"
-                as="h2">
-                {value.name}
-            </Heading>
-            <Text>Price: {value.price} AMD</Text>
-            <Text>Type: {value.type}</Text>
-        </Stack>
-      </Box>
+    <Box
+      borderRadius="8px"
+      overflow="hidden"
+      boxShadow="0 0 24px 4px rgba(0, 0, 0, 0.15)"
+    >
+      <Image
+        src="/assets/images/photo.jpg"
+        w="full"
+        objectFit="cover"
+        h="200px"
+      />
+      <Stack p="4" bg="yellow.100">
+        <Heading fontSize="3xl" as="h2">
+          {value.name ? value.name : value.title}
+        </Heading>
+        <Text>Price: {value.price} AMD</Text>
+        <Text>Type: {value.type}</Text>
+      </Stack>
+    </Box>
   );
 };
 
--- src/pages/config.js
+++ src/pages/config.js
@@ -1,13 +1,14 @@
-import AuthLayout from "../layouts/AuthLayout"
-import DefaultLayout from "../layouts/DefaultLayout"
-import Homepage from "./Homepage"
-import Register from "./Register"
-import Login from "./Login"
-import Verify from "./Verify"
-import ForgottenPassword from "./ForgotPassword"
-import ResetPassword from "./ResetPassword"
-import CreateProduct from "./CreateProduct"
-import Search from './Search'
+import AuthLayout from "../layouts/AuthLayout";
+import DefaultLayout from "../layouts/DefaultLayout";
+import Homepage from "./Homepage";
+import Register from "./Register";
+import Login from "./Login";
+import Verify from "./Verify";
+import ForgottenPassword from "./ForgotPassword";
+import ResetPassword from "./ResetPassword";
+import CreateProduct from "./CreateProduct";
+import Search from "./Search";
+import MyProducts from "./MyProducts";
 
 const config = [
   {
@@ -54,10 +55,16 @@ const config = [
   },
   {
     component: CreateProduct,
-    path: '/create-product',
+    path: "/create-product",
     secure: false,
     layout: AuthLayout,
-   }
-]
+  },
+  {
+    component: MyProducts,
+    path: "/myproducts",
+    secure: false,
+    layout: DefaultLayout,
+  },
+];
 
 export default config;
