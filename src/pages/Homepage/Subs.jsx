88a3acf05d1b7f5974492d731077f06c86fbb474
koryun <koryun.ohanyan@gmail.com>
2021-10-08 15:12:52 +0400

    add variant to button

--- src/pages/Homepage/Subs.jsx
+++ src/pages/Homepage/Subs.jsx
@@ -3,14 +3,14 @@ import { Box, Flex, Text, Button, Input } from "@chakra-ui/react";
 const Subscribe = () => {
   return (
     <Box maxW="full" bg="gray.100" border="1px solid black" mt="10" p="5">
-      <Flex justifyContent="space-arround">
+      <Flex justifyContent="space-between">
         <Text size="xl">
           Подпишитесь на рассылку и будьте в курсе! Акции, скидки, распродажи
           ждут!
         </Text>
 
         <Input placeholder="enter your email" w="2xl" />
-        <Button bg="red">subscribe</Button>
+        <Button variant="red">subscribe</Button>
       </Flex>
     </Box>
   );
