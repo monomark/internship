44ca8a3f9e436e30ff4aec1dc0dbf9b3ffafe56b
Hayk <hayksargsyan0909@gmail.com>
2021-10-13 17:51:13 +0300

    create export useCreateProducts

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
-export { default as useUser } from './useUser'
-export { default as useGetProject } from './project/useGetProject'
+export { default as useUser } from "./useUser";
+export { default as useGetProject } from "./project/useGetProject";
+export { default as useCreateProduct } from "./product/useCreateProduct";
